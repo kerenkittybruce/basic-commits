4ac18ad78807bf5b5abc0d78652efbb8fd39b34c
kerenkittybruce <kerenchelseabruce@gmail.com>
2022-11-23 12:03:02 +0200

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -1,22 +1,28 @@
+// Javascript for Calculator Task
+
+//To display input values
 let result = document.getElementById("inputext");
 
+// To calculate equations
 let calculate = (number) => {
   result.value = result.value + number;
-}
+};
 
+// To display results of equations
 let Result = () => {
   try {
-    result.value = eval(result.value)
-  }
-  catch(err){
+    result.value = eval(result.value);
+  } catch (err) {
     alert("Enter the valid Input");
   }
-}
+};
 
-function clr(){
+//To clear all input values
+function clr() {
   result.value = " ";
 }
 
-function del(){
+//To act as backspace / clear one digit
+function del() {
   result.value = result.value.slice(0, -1);
 }
