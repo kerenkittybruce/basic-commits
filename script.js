ad5b0811e0bc607838fb4ab4b750346085aeee2d
kerenkittybruce <kerenchelseabruce@gmail.com>
2022-11-26 11:45:28 +0200

    Editing Media Queries
    Added media queries for small mobile screens, as well as laptops.

---
+++ script.js
@@ -0,0 +1,34 @@
+// Javascript for Calculator Task
+
+//To display input values
+let sum = document.getElementById("inputext");
+
+// To input equations to calculate
+let calculate = (number) => {
+  sum.value = sum.value + number;
+};
+
+let addToSum = (operator) => {
+  sum.value = sum.value + operator;
+};
+
+// To display results of equations
+let evaluateSum = () => {
+  try {
+    sum.value = eval(sum.value);
+  } catch (err) {
+    alert(
+      "Oops! 🤭 There seems to be an issue here... 🤔💭 Please enter the valid input values."
+    );
+  }
+};
+
+//To clear all input values
+function clearSum() {
+  sum.value = " ";
+}
+
+//To act as backspace / clear one digit
+function deleteSum() {
+  sum.value = sum.value.slice(0, -1);
+}
